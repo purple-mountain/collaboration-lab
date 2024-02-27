b4e9bd2b068a40116246dd00be399d0a196a4488
purple-mountain <lmwrong92@gmail.com>
2024-02-27 23:19:15 +0500

    Fixed typo in sign in page

--- frontend/src/routes/sign-in.tsx
+++ frontend/src/routes/sign-in.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export default function SignInPage() {
   return (
     <div className="flex h-screen justify-center items-center bg-white text-black antialiased dark:bg-bodyBackground dark:text-slate-200">
-      <SignIn />;
+      <SignIn />
     </div>
   );
 }
--- frontend/src/routes/sign-up.tsx
+++ frontend/src/routes/sign-up.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 export default function SignUpPage() {
   return (
     <div className="flex h-screen justify-center items-center bg-white text-black antialiased dark:bg-bodyBackground dark:text-slate-200">
-      <SignUp />;
+      <SignUp />
     </div>
   );
 }
